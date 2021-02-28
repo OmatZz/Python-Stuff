b47a1d2af49e8838aaa3300bad4c2c1d958400f8
OmatZz <omatz@comunidad.unam.mx>
2021-02-27 23:13:20 -0600

    string manipulation

---
+++ Excercises/fruit.py
@@ -0,0 +1,11 @@
+#checking spelling of the user
+
+while True :
+    ufruit = input('Enter banana : ')
+    fruit = 'banana'
+    if ufruit == fruit :
+        print('nice')
+        break
+    else :
+        print ('not nice')
+        continue
