8c5eca250ed13ddbd382fedccd2c3b890dff5dee
OmatZz <omatz@comunidad.unam.mx>
2021-03-06 19:27:24 -0600

    added some comments

--- Simple stuff/loops.py
+++ Simple stuff/loops.py
@@ -1,7 +1,7 @@
 #smallest number with loop
 smallest=None
 for value in [15,35,86,1,96,-95,-100]:
-    if smallest is None:
+    if smallest is None: #is, used on None, true/false operations
         smallest=value
     elif value < smallest:
         smallest=value
